194e544c9a890ae9f32ba598d3b12769dc3ec6b4
bpetryshyn <bpetryshyn@hubspot.com>
2020-03-30 23:05:52 +0300

    Extract message subscriber

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-const AMQP_HOST = process.env.AMQP_HOST || 'localhost';
-const AMQP_URL = `amqp://${AMQP_HOST}`;
+const startMessageSubscriber = require('./subscriber/startMessageSubscriber');
 
-const spcAmqpClient = require('@bpetryshyn/spc-amqp-client');
-
-const storeMessage = require('./storeMessage');
-
-const startApp = async () => {
-  const amqpClient = await spcAmqpClient(AMQP_URL);
-
-  amqpClient.createPersistentMessageConsumer(
-    'history',
-    async (message, ack) => {
-      await storeMessage(message);
-      console.log('Message persisted: ', message);
-      ack();
-    }
-  );
-};
-
-startApp();
+startMessageSubscriber();
